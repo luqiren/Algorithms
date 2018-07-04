cdd7e71f1509f5b21502a241940e90d31b7783d5
luqiren <luqiren>
2018-07-04 15:31:13 +0800

    update

---
+++ Designer/src/design/TriangleOfYangHui.java
@@ -0,0 +1,13 @@
+package design;
+
+import java.util.List;
+
+public class TriangleOfYangHui {
+    public static void main (String[] args) {
+        int n = 5;
+    }
+    public List<List<Integer>> generate(int numRows) {
+        return null;
+    }
+}
+
